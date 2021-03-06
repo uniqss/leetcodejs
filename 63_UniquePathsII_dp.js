8a9e9abb4c78a229b7a9a874a3b094a2f8bd7980
uniqss <uniqs@163.com>
2021-03-06 16:40:31 +0800

    ww

---
+++ 63_UniquePathsII_dp.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    if (obstacleGrid[0][0] === 1) return 0;
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = obstacleGrid.slice();
+    dp[0][0] = 1;
+    for (let i = 1;i < m;i++) {
+        dp[i][0] = (dp[i - 1][0] > 0 && obstacleGrid[i][0] === 0) ? 1 : 0;
+    }
+    for (let j = 1;j < n;j++) {
+        dp[0][j] = (dp[0][j - 1] > 0 && obstacleGrid[0][j] === 0) ? 1 : 0;
+    }
+    for (let i = 1;i < m;i++) {
+        for (let j = 1;j < n;j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+};
+
+
+let obstacleGrid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+
+let result = uniquePathsWithObstacles(obstacleGrid);
+console.log(result);
